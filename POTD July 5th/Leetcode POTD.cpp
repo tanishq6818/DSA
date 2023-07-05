9b79ba2692f8eadaf86f99dab2918088dedcab3d
tanishq6818 <76662143+tanishq6818@users.noreply.github.com>
2023-07-05 15:59:36 +0530

    Create Leetcode POTD.cpp

---
+++ POTD July 5th/Leetcode POTD.cpp
@@ -0,0 +1,43 @@
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1]
+Output: 3
+Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
+Example 2:
+
+Input: nums = [0,1,1,1,0,1,1,0,1]
+Output: 5
+Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+Example 3:
+
+Input: nums = [1,1,1]
+Output: 2
+Explanation: You must delete one element.
+
+int longestSubarray(vector<int>& nums) {
+        // Number of zero's in the window.
+        int zeroCount = 0;
+        int longestWindow = 0;
+        // Left end of the window.
+        int start = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            zeroCount += (nums[i] == 0);
+                          
+            // Shrink the window until the zero counts come under the limit.
+            while (zeroCount > 1) {
+                zeroCount -= (nums[start] == 0);
+                start++;
+            }
+              
+            longestWindow = max(longestWindow, i - start);
+        }
+
+        return longestWindow;
+    }
