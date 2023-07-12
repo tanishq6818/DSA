ad121f624846f01c9251dca055f3e1d0da136754
tanishq6818 <76662143+tanishq6818@users.noreply.github.com>
2023-07-13 00:56:58 +0530

    Update POTD CN.cpp

--- POTD July 13th/POTD CN.cpp
+++ POTD July 13th/POTD CN.cpp
@@ -14,3 +14,22 @@ int CountSumConsecutiveNums(int n)
     }
     return count;
 }
+//optimized 
+
+#include <cmath>
+
+int CountSumConsecutiveNums(int n) {
+    int count = 0;
+    int limit = (int)sqrt(2 * n);
+
+    for (int i = 1; i <= limit; i++) {
+        int numerator = n - (i * (i - 1)) / 2;
+        int denominator = i;
+
+        if (numerator % denominator == 0) {
+            count++;
+        }
+    }
+
+    return count-1;
+}
