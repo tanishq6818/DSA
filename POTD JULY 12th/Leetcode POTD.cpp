b2062bccb7c7411d460d06115ad053e2191f5e7b
tanishq6818 <76662143+tanishq6818@users.noreply.github.com>
2023-07-12 12:44:46 +0530

    Update Leetcode POTD.cpp

--- POTD JULY 12th/Leetcode POTD.cpp
+++ POTD JULY 12th/Leetcode POTD.cpp
@@ -31,7 +31,49 @@
 // graph[i] is sorted in a strictly increasing order.
 // The graph may contain self-loops.
 // The number of edges in the graph will be in the range [1, 4 * 104].
+//Optimized One
 
+bool check(int v, vector<int> &vis, vector<int> &pathVis, vector<vector<int>> &graph, vector<int> &safeNodes){
+        vis[v] = 1;
+        pathVis[v] = 1;
+        for(auto i : graph[v]){
+            if(!vis[i]){
+                    if(check(i,vis,pathVis,graph,safeNodes)==true){
+                        safeNodes[i]=0;
+                        return true;
+                    }
+                }
+                if(pathVis[i]){
+                    safeNodes[i]=0;
+                    return true;
+                }
+        }
+        pathVis[v] = 0;
+        safeNodes[v] = 1;
+        return false;
+    }
+
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<int> vis(V,0);
+        vector<int> pathVis(V,0);
+        vector<int> safeNodes(V,0);
+        vector<int> ans;
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                check(i,vis,pathVis,graph,safeNodes);
+            }
+        }
+        for(int i=0;i<V;i++){
+            if(safeNodes[i]==1){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+
+//Brute Force
 vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> indegree(n);
