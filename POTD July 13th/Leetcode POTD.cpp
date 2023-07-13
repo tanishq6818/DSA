b21f354a836d5fb193de8d5fdba1ed9e9d57c23d
tanishq6818 <76662143+tanishq6818@users.noreply.github.com>
2023-07-13 13:00:17 +0530

    Create Leetcode POTD.cpp

---
+++ POTD July 13th/Leetcode POTD.cpp
@@ -0,0 +1,59 @@
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return true if you can finish all courses. Otherwise, return false.
+
+ 
+
+// Example 1:
+
+// Input: numCourses = 2, prerequisites = [[1,0]]
+// Output: true
+// Explanation: There are a total of 2 courses to take. 
+// To take course 1 you should have finished course 0. So it is possible.
+// Example 2:
+
+// Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+// Output: false
+// Explanation: There are a total of 2 courses to take. 
+// To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit, vector<bool>& inStack) {
+        // If the node is already in the stack, we have a cycle.
+        if (inStack[node]) {
+            return true;
+        }
+        if (visit[node]) {
+            return false;
+        }
+        // Mark the current node as visited and part of current recursion stack.
+        visit[node] = true;
+        inStack[node] = true;
+        for (auto neighbor : adj[node]) {
+            if (dfs(neighbor, adj, visit, inStack)) {
+                return true;
+            }
+        }
+        // Remove the node from the stack.
+        inStack[node] = false;
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for (auto prerequisite : prerequisites) {
+            adj[prerequisite[1]].push_back(prerequisite[0]);
+        }
+
+        vector<bool> visit(numCourses);
+        vector<bool> inStack(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, adj, visit, inStack)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
