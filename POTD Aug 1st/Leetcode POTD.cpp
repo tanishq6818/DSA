b1f68ecb2ec3a7f51f7538d4e3bbf43821f02539
Tanishq Agarwal <76662143+tanishq6818@users.noreply.github.com>
2023-08-01 12:08:49 +0530

    Create Leetcode POTD.cpp

---
+++ POTD Aug 1st/Leetcode POTD.cpp
@@ -0,0 +1,49 @@
+// Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+
+// You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: n = 4, k = 2
+// Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+// Explanation: There are 4 choose 2 = 6 total combinations.
+// Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+// Example 2:
+
+// Input: n = 1, k = 1
+// Output: [[1]]
+// Explanation: There is 1 choose 1 = 1 total combination.
+ 
+
+// Constraints:
+
+// 1 <= n <= 20
+// 1 <= k <= n 
+
+
+
+
+#include <vector>
+using namespace std;
+
+void backtrack(int n, int k, int start, vector<int>& current, vector<vector<int>>& result) {
+    if (current.size() == k) {
+        result.push_back(current);
+        return;
+    }
+
+    for (int i = start; i <= n; ++i) {
+        current.push_back(i);
+        backtrack(n, k, i + 1, current, result);
+        current.pop_back();
+    }
+}
+
+vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> result;
+    vector<int> current;
+    backtrack(n, k, 1, current, result);
+    return result;
+}
