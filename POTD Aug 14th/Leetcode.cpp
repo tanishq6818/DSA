f0fc88a5042f762224cbe060e3a19eb515cd165b
Tanishq Agarwal <76662143+tanishq6818@users.noreply.github.com>
2023-08-14 15:44:43 +0530

    Create Leetcode.cpp

---
+++ POTD Aug 14th/Leetcode.cpp
@@ -0,0 +1,11 @@
+int findKthLargest(vector<int>& nums, int k) {
+         priority_queue<int> heap;
+        for (int num: nums) {
+            heap.push(-num);
+            if (heap.size() > k) {
+                heap.pop();
+            }
+        }
+        
+        return -heap.top();
+    }
