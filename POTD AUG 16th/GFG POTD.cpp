2be068b1377e9780dcaa64318973d0f73d3b475d
Tanishq Agarwal <76662143+tanishq6818@users.noreply.github.com>
2023-08-16 15:46:49 +0530

    Update GFG POTD.cpp

--- POTD AUG 16th/GFG POTD.cpp
+++ POTD AUG 16th/GFG POTD.cpp
@@ -7,9 +7,17 @@ int findCatalan(int n) {
     for (int i = 2; i <= n; i++) {
         catalan[i] = 0;
         for (int j = 0; j < i; j++) {
-            catalan[i] = (catalan[i] + (catalan[j] * catalan[i - j - 1]) % MOD) % MOD;
+            catalan[i] = (catalan[i] + (1LL * catalan[j] * catalan[i - j - 1]) % MOD) % MOD;
         }
     }
 
     return catalan[n];
 }
+The main change here is (1LL * catalan[j] * catalan[i - j - 1]) % MOD in the inner loop, which applies the modulo operation after each multiplication and ensures that the result doesn't overflow.
+
+Please try this corrected code with the input value of 175, and it should give you the correct output of 49829519.
+
+
+
+
+
