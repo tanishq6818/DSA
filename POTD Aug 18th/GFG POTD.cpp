75bd2d3fb4ff0bfaee55a8452fc56c3f7cd2142a
Tanishq Agarwal <76662143+tanishq6818@users.noreply.github.com>
2023-08-18 12:21:26 +0530

    Create GFG POTD.cpp

---
+++ POTD Aug 18th/GFG POTD.cpp
@@ -0,0 +1,50 @@
+// Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
+
+// Example 1:
+
+// Input:
+// n = 6
+// A[] = {16,17,4,3,5,2}
+// Output: 17 5 2
+// Explanation: The first leader is 17 
+// as it is greater than all the elements
+// to its right.  Similarly, the next 
+// leader is 5. The right most element 
+// is always a leader so it is also 
+// included.
+// Example 2:
+
+// Input:
+// n = 5
+// A[] = {1,2,3,4,0}
+// Output: 4 0
+// Explanation: 0 is the rightmost element
+// and 4 is the only element which is greater
+// than all the elements to its right.
+// Your Task:
+// You don't need to read input or print anything. The task is to complete the function leader() which takes array A and n as input parameters and returns an array of leaders in order of their appearance.
+
+// Expected Time Complexity: O(n)
+// Expected Auxiliary Space: O(n)
+
+// Constraints:
+// 1 <= n <= 107
+// 0 <= Ai <= 107
+
+
+vector<int> leaders(int a[], int n){
+        // Code here
+        vector<int> result;
+        int maxRight = a[n - 1]; // The rightmost element is always a leader
+        result.push_back(maxRight);
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (a[i] >= maxRight) {
+                maxRight = a[i];
+                result.push_back(maxRight);
+            }
+        }
+        
+        reverse(result.begin(), result.end()); // Leaders were added in reverse order
+        return result;
+    }
