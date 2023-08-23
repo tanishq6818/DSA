2d36286bd31ea2af38e7c23a5675724c58a8b98a
Tanishq Agarwal <76662143+tanishq6818@users.noreply.github.com>
2023-08-23 22:10:25 +0530

    Create Leetcode POTD.cpp

---
+++ POTD Aug 23th/Leetcode POTD.cpp
@@ -0,0 +1,60 @@
+// Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+// Return any possible rearrangement of s or return "" if not possible.
+
+ 
+
+// Example 1:
+
+// Input: s = "aab"
+// Output: "aba"
+// Example 2:
+
+// Input: s = "aaab"
+// Output: ""
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 500
+// s consists of lowercase English letters.
+
+string reorganizeString(string s) {
+        vector<int> charCounts(26, 0);
+        for (char c : s) {
+            charCounts[c - 'a'] = charCounts[c - 'a'] + 1;
+        }
+
+        // Max heap ordered by character counts
+        priority_queue<vector<int>> pq;
+        for (int i = 0; i < 26; i++) {
+            if (charCounts[i] > 0) {
+                pq.push(vector<int>{charCounts[i], i + 'a'});
+            }
+        }
+        
+        string result;
+        while (!pq.empty()) {
+            auto first = pq.top();
+            pq.pop();
+            if (result.empty() || first[1] != result.back()) {
+                result += char(first[1]);
+                if (--first[0] > 0) {
+                    pq.push(first);
+                }
+            } else {
+                if (pq.empty()) {
+                    return "";
+                }
+                auto second = pq.top();
+                pq.pop();
+                result += char(second[1]);
+                if (--second[0] > 0) {
+                    pq.push(second);
+                }
+                pq.push(first);
+            }
+        }
+
+        return result;
+    }
