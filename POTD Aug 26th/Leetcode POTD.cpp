510c1546dea816e7a090514dc5de6a1038001a1f
Tanishq Agarwal <76662143+tanishq6818@users.noreply.github.com>
2023-08-26 13:16:31 +0530

    Update Leetcode POTD.cpp

--- POTD Aug 26th/Leetcode POTD.cpp
+++ POTD Aug 26th/Leetcode POTD.cpp
@@ -19,28 +19,43 @@
 // Output: 3
 // Explanation: The longest chain is [1,2] -> [4,5] -> [7,8].
  
-
-int longestPairChain(int i, vector<vector<int>>& pairs, int& n, vector<int>& memo) {
-        if (memo[i] != 0) {
-            return memo[i];
-        }
-        memo[i] = 1;
-        for (int j = i + 1; j < n; i++) {
-            if (pairs[i][1] < pairs[j][0]) {
-                memo[i] = max(memo[i], 1 + longestPairChain(j, pairs, n, memo));
-            }
-        }
-        return memo[i];
-    }
-
-    int findLongestChain(vector<vector<int>>& pairs) {
+int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
         sort(pairs.begin(), pairs.end());
-        vector<int> memo(n);
+        vector<int> dp(n, 1);
+        int ans = 1;
 
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, longestPairChain(i, pairs, n, memo));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (pairs[i][1] < pairs[j][0]) {
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
+            }
+            ans = max(ans, dp[i]);
         }
         return ans;
     }
+// int longestPairChain(int i, vector<vector<int>>& pairs, int& n, vector<int>& memo) {
+//         if (memo[i] != 0) {
+//             return memo[i];
+//         }
+//         memo[i] = 1;
+//         for (int j = i + 1; j < n; i++) {
+//             if (pairs[i][1] < pairs[j][0]) {
+//                 memo[i] = max(memo[i], 1 + longestPairChain(j, pairs, n, memo));
+//             }
+//         }
+//         return memo[i];
+//     }
+
+//     int findLongestChain(vector<vector<int>>& pairs) {
+//         int n = pairs.size();
+//         sort(pairs.begin(), pairs.end());
+//         vector<int> memo(n);
+
+//         int ans = 0;
+//         for (int i = 0; i < n; i++) {
+//             ans = max(ans, longestPairChain(i, pairs, n, memo));
+//         }
+//         return ans;
+//     }
