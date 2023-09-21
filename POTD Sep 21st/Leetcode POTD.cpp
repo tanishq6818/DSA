dfed8f924625a18653b3638773e04aadc34eea3b
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-09-21 13:53:45 +0530

    Update Leetcode POTD.cpp

--- POTD Sep 21st/Leetcode POTD.cpp
+++ POTD Sep 21st/Leetcode POTD.cpp
@@ -14,6 +14,41 @@
 // Input: nums1 = [1,2], nums2 = [3,4]
 // Output: 2.50000
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+       vector<int>v;
+       int i=0,j=0;
+
+       while(i<nums1.size() && j<nums2.size())
+       {
+           if(nums1[i]>nums2[j])
+           {
+               v.push_back(nums2[j]);
+                j++;
+           }
+           else
+           {
+               v.push_back(nums1[i]);
+               i++;
+           }
+       }
+       while(i<nums1.size())
+       {
+            v.push_back(nums1[i]);
+               i++;
+       }
+       while(j<nums2.size())
+           {
+               v.push_back(nums2[j]);
+                j++;
+           }
+        
+        int n=v.size();
+        if(n%2!=0)
+        return v[n/2];
+        return (v[n/2]+v[n/2-1])/2.0;
+ }
  
 
 class Solution {
