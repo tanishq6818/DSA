174a2f9072de170d07d5f6c50d7054d49d6e1c5d
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-10-09 20:42:54 +0530

    Create Leetcode POTD.cpp

---
+++ POTD Oct 8th/Leetcode POTD.cpp
@@ -0,0 +1,64 @@
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109 
+  vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> result = {-1, -1};
+    int left = 0, right = nums.size() - 1;
+
+    // Find the left boundary
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result[0] = mid;
+            right = mid - 1;  // Continue searching on the left side
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    // Reset left and right pointers
+    left = 0;
+    right = nums.size() - 1;
+
+    // Find the right boundary
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result[1] = mid;
+            left = mid + 1;   // Continue searching on the right side
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+}
