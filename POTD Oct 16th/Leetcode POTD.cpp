8d7af452373aeb74219139e2f9a13543845970ed
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-10-16 16:24:49 +0530

    Create Leetcode POTD.cpp

---
+++ POTD Oct 16th/Leetcode POTD.cpp
@@ -0,0 +1,35 @@
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+// Example 1:
+
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+
+// Input: rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+
+vector<int> getRow(int rowIndex) {
+    std::vector<int> result;
+    
+    for (int i = 0; i <= rowIndex; i++) {
+        // Add 1 at the beginning of the row
+        result.insert(result.begin(), 1);
+        
+        // Update the row based on the previous row
+        for (int j = 1; j < i; j++) {
+            result[j] = result[j] + result[j + 1];
+        }
+    }
+    
+    return result;
+}
