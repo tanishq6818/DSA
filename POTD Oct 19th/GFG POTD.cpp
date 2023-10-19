65e8e212427bc2ae362dd2ba5683fe32242c328b
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-10-19 14:09:23 +0530

    Create GFG POTD.cpp

---
+++ POTD Oct 19th/GFG POTD.cpp
@@ -0,0 +1,67 @@
+// Given an integer X and an undirected acyclic graph with V nodes, labeled from 0 to V-1, and E edges, find the level of node labeled as X.
+
+// Level is the minimum number of edges you must travel from the node 0 to some target.
+
+// If there doesn't exists such a node that is labeled as X, return -1.
+
+// Example 1:
+
+// Input:
+
+// X = 4
+// Output:
+// 2
+// Explanation:
+
+// We can clearly see that Node 4 lies at Level 2.
+// Example 2:
+
+// Input:
+
+// X = 1
+// Output:
+// 1
+// Explanation:
+// Node 1 lies at level 1, immediate after Node 0.
+// Your task:
+// You dont need to read input or print anything. Your task is to complete the function nodeLevel() which takes two integers V and X denoting the number of vertices and the node, and another adjacency list adj as input parameters and returns the level of Node X. If node X isn't present it returns -1.
+
+// Expected Time Complexity: O(V)
+// Expected Auxiliary Space: O(V)
+
+// Constraints:
+// 2 ≤ V ≤ 104
+// 1 ≤ E ≤ 104
+// 0 ≤ adji, j < V
+// 1 ≤ X < V
+// Graph doesn't contain multiple edges and self loops
+
+
+int nodeLevel(int V, vector<int> adj[], int X) {
+    vector<int> visited(V, false);
+    vector<int> level(V, 0);
+
+    queue<int> q;
+    q.push(0);
+    visited[0] = true;
+    level[0] = 0;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for (int v : adj[u]) {
+            if (!visited[v]) {
+                q.push(v);
+                visited[v] = true;
+                level[v] = level[u] + 1;
+            }
+        }
+    }
+
+    if (level[X] == 0) {
+        return -1;
+    }
+
+    return level[X];
+}
