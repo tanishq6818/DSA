58ecfde5931dc01863b39f7c3fb321c0d2014d76
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-10-27 10:08:36 +0530

    Create Leetcode POTD.cpp

---
+++ POTD Oct 27th/Leetcode POTD.cpp
@@ -0,0 +1,56 @@
+// Given a string s, return the longest 
+// palindromic
+ 
+// substring
+//  in s.
+
+ 
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+
+  string longestPalindrome(string s) {
+        int start = 0, maxLength = 1;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            // Odd length palindromes
+            int left = i, right = i;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLength) {
+                    start = left;
+                    maxLength = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+
+            // Even length palindromes
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 > maxLength) {
+                    start = left;
+                    maxLength = right - left + 1;
+                }
+                left--;
+                right++;
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
