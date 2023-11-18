aa1e28900a37dfbb60721ee151b36fdc34efa061
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-11-18 22:40:25 +0530

    Create GFG POTD.cpp

---
+++ POTD Nov 18th/GFG POTD.cpp
@@ -0,0 +1,64 @@
+// Given a doubly linked list of n elements. Your task is to reverse the doubly linked list in-place.
+
+// Example 1:
+
+// Input:
+// LinkedList: 3 <--> 4 <--> 5
+// Output: 5 4 3
+// Example 2:
+
+// Input:
+// LinkedList: 75 <--> 122 <--> 59 <--> 196
+// Output: 196 59 122 75
+// Your Task:
+// Your task is to complete the given function reverseDLL(), which takes head reference as argument and this function should reverse the elements such that the tail becomes the new head and all pointers are pointing in the right order. You need to return the new head of the reversed list. The printing and verification is done by the driver code.
+
+// Expected Time Complexity: O(n).
+// Expected Auxiliary Space: O(1).
+
+// Constraints:
+// 1 <= number of nodes <= 104
+// 0 <= value of nodes <= 104
+
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+class Solution
+{
+    public:
+    Node* reverseDLL(Node * head)
+    {
+        Node* current = head;
+        Node* temp = NULL;
+
+        while (current != NULL) {
+            // Swap next and prev pointers for the current node
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+
+            // Move to the next node
+            current = current->prev;
+        }
+
+        // Check if the list is not empty
+        if (temp != NULL) {
+            // Set the new head
+            head = temp->prev;
+        }
+
+        return head;
+    }
+};
