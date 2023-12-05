fb408866a12c608949136cfc690ec990f39c13df
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-12-05 19:38:24 +0530

    Update BitMagic.cpp

--- GFG DSA/BitMagic.cpp
+++ GFG DSA/BitMagic.cpp
@@ -2,6 +2,7 @@
 // x=-1-x
 //IN unsigned RIGHT SHIFT OPerator ,for negative numbers,  the leading bits will be filled by 1 instead of 0 (>>>)
 
+//Checking for the Kth Bit
 
 //LEFT SHIFT OPERATOR
 void checkKthBit(int n){
@@ -21,4 +22,85 @@ void checkKthBit(int n){
     else if((n>>(k-1))&1!=0){
         cout<<"Bit is One";
     }
+}
+
+
+//Counting of the Bits
+
+int countSet(int n){
+    int count=0;
+    while(n>0){
+        count+=(n&1);
+        n>>=1;
+    }
+}
+
+//Look Up table method for numbers range from 0 to 255(DP) 
+//It splits the 32 bits in the chunk of 8 bits
+
+int table[256];
+void initialize(){
+    table[0]=0;
+    for(int i=1;i<256;i++){
+        table[i]=(i&1 )+ table[i/2];
+    }
+}
+int countBits(int n)
+{
+    int res=table[n & 0xff];            //0xff denotes the value of Last 8 bits
+    n>>=8;
+    res+=table[n & 0xff];
+    n>>=8;
+    res+=table[n & 0xff];
+    n>>=8;
+    res+=table[n & 0xff];
+    return res;
+}
+
+
+
+//check Power of 2
+
+void checkPowerof2(int n){
+    if(n==0){
+        return;
+    }
+    while(n!=1){
+        if(n%2!=0)  {
+            cout<<"Not a Power of 2";
+            break;}
+        n/=2;
+    }
+    cout<<"Power Of 2";
+}
+
+bool chkPowerof2(int n){                //Optimized solution
+    if(n==0){
+        return false;
+    }
+    return n&(n-1)==0;
+}
+
+
+
+//Find Odd Occuring Number
+
+int odd(int n,int arr[]){
+    int res=0;
+    for(int i=0;i<n;i++){
+        res^=arr[i];
+    }
+    return res;
+}
+
+
+int missing(int n,int arr[]){
+    int res=0;
+    for(int i=1;i<=n;i++){
+        res^=i;
+    }
+    for(int i=0;i<n;i++){
+        res^=arr[i];
+    }
+    return res;
 }
