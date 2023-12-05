0772e7076fb79da3e35c0da32c845db8417641ba
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2023-12-05 16:37:19 +0530

    Update Prime(MATHS).cpp

--- GFG DSA/Prime(MATHS).cpp
+++ GFG DSA/Prime(MATHS).cpp
@@ -8,7 +8,7 @@ int prime(int n){
     return 1;
 }
 
-bool prime2(int n){
+bool prime2(int n){                             //Best Optimized
     if(n==1)    return false;
     if(n==2 || n==3)    return true;
     if(n%2==0 || n%3==0)    return false;
@@ -18,4 +18,54 @@ bool prime2(int n){
         }
     }
     return true;
+}
+
+void primefactors(int n){
+    for(int i=2;i<=n;i++){
+        if(prime2(i)){
+            int x=i;
+            while(n%i==0){
+                cout<<i<<" ";
+                x*=i;
+            }
+        }
+    }
+}
+
+void primefactors2(int n){            //Optimized
+    if(n<=1)    return;
+    for(int i=2;i*i<=n;i++){
+        while(n%i==0){
+            cout<<i;
+            n/=i;
+        }
+    }
+    if(n>1){
+        cout<<n;
+    }
+}
+
+void primefactors3(int n){                    //Best Optimization
+    if(n<=1)    return;
+    while(n%2==0){
+        cout<<2<<" ";
+        n/=2;
+    }
+    while(n%3==0){
+        cout<<3<<" ";
+        n/=3;
+    }
+    for(int i=5;i*i<=n;i+=6){
+        while(n%i==0){
+            cout<<i<<" ";
+            n/=i;
+        }
+        while(n%(i+2)==0){
+            cout<<i+2<<" ";
+            n/=(i+2);
+        }
+    }
+    if(n>3){
+        cout<<n<<" ";
+    }
 }
