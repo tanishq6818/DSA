14583401650b4af7e1231101d51a825df4847496
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-01-11 11:57:17 +0530

    Create Leetcode POTD.cpp

---
+++ POTD Jan 11th/Leetcode POTD.cpp
@@ -0,0 +1,52 @@
+Given the root of a binary tree, find the maximum value v for which there exist different nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
+
+A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
+
+ 
+
+Example 1:
+
+
+Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
+Output: 7
+Explanation: We have various ancestor-node differences, some of which are given below :
+|8 - 3| = 5
+|3 - 7| = 4
+|8 - 1| = 7
+|10 - 13| = 3
+Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+Example 2:
+
+
+Input: root = [1,null,2,null,0,3]
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 5000].
+0 <= Node.val <= 105
+
+  class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root) {
+        return dfs(root, INT_MAX, INT_MIN);
+    }
+
+private:
+    int dfs(TreeNode* node, int minValue, int maxValue) {
+        if (node == nullptr) {
+            return maxValue - minValue;
+        }
+
+        // Update the minimum and maximum values
+        minValue = min(minValue, node->val);
+        maxValue = max(maxValue, node->val);
+
+        // Calculate differences considering the current node
+        int leftDiff = dfs(node->left, minValue, maxValue);
+        int rightDiff = dfs(node->right, minValue, maxValue);
+
+        return max(leftDiff, rightDiff);
+    }
+};
