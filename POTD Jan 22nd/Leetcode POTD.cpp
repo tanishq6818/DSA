bec5fdd7c113f07af7f581b32bd026b0461aac42
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-01-22 17:25:39 +0530

    Create Leetcode POTD.cpp

---
+++ POTD Jan 22nd/Leetcode POTD.cpp
@@ -0,0 +1,44 @@
+// You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+// You are given an integer array nums representing the data status of this set after the error.
+
+// Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,2,4]
+// Output: [2,3]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [1,2]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// 1 <= nums[i] <= 104
+
+vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> result(2, 0);
+        unordered_set<int> numSet;
+        int n = nums.size();
+        int originalSum = n * (n + 1) / 2;
+        int currentSum = 0;
+
+        for (int num : nums) {
+            // Find the duplicate number
+            if (numSet.count(num) == 1) {
+                result[0] = num;
+            }
+            numSet.insert(num);
+            currentSum += num;
+        }
+
+        // Find the missing number
+        result[1] = result[0] + (originalSum - currentSum);
+
+        return result;
+    }
