d8845414fdb61ca4d10898cc7ebe495b7ba45868
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-02-28 22:40:38 +0530

    Create Leetcode POTD.cpp

---
+++ POTD FEB 28th/Leetcode POTD.cpp
@@ -0,0 +1,54 @@
+// Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [2,1,3]
+// Output: 1
+// Example 2:
+
+
+// Input: root = [1,2,3,4,null,5,6,null,null,7]
+// Output: 7
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 104].
+// -231 <= Node.val <= 231 - 1
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> queue;
+        TreeNode* current = root;
+        queue.push(current);
+
+        while (!queue.empty()) {
+            current = queue.front();
+            queue.pop();
+            if (current->right != nullptr) {
+                queue.push(current->right);
+            }
+            if (current->left != nullptr) {
+                queue.push(current->left);
+            }
+        }
+        return current->val;
+    }
+};
+
+
