b1af9d7e06170c11a90a7e0957ff4bd114fd54ed
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-03-08 14:38:43 +0530

    Create Leetcode POTD.cpp

---
+++ POTD March 8th/Leetcode POTD.cpp
@@ -0,0 +1,52 @@
+You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+  class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        // Find the frequency of each element
+        unordered_map<int, int> frequencies;
+        for (int num : nums) {
+            frequencies[num]++;
+        }
+
+        // Determine the maximum frequency
+        int maxFrequency = 0;
+        for (auto [num, frequency] : frequencies) {
+            maxFrequency = max(maxFrequency, frequency);
+        }
+
+        // Calculate the total frequencies of elements with the maximum frequency
+        int frequencyOfMaxFrequency = 0;
+        for (auto [num, frequency] : frequencies) {
+            if (frequency == maxFrequency) {
+                frequencyOfMaxFrequency++;
+            }
+        }
+        return frequencyOfMaxFrequency * maxFrequency;
+    }
+};
