9d3cb32a769b3ecb8ec89eda7df4dddfebc6b061
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-03-14 19:36:08 +0530

    Update GFG POTD.cpp

--- POTD MARCH 14TH/GFG POTD.cpp
+++ POTD MARCH 14TH/GFG POTD.cpp
@@ -43,28 +43,54 @@
 // 1 <= n <= 1000
 // a[i][j] belongs to {'X','O'} 
 
-int largestSubsquare(int n, vector<vector<char>> a) {
-    // Initialize an auxiliary matrix to store sizes of largest square subgrids
-    vector<vector<int>> dp(n, vector<int>(n, 0));
-    int maxSize = 0;
+class Solution {
+  public:
+    // Function to find the largest subsquare with 'X'
+    int largestSubsquare(int n, vector<vector<char>> a) {
+        int by_rows[n][n];
+        int by_cols[n][n];
 
-    // Fill the first row and first column of dp matrix with values from input matrix
-    for (int i = 0; i < n; ++i) {
-        dp[i][0] = (a[i][0] == 'X') ? 1 : 0;
-        dp[0][i] = (a[0][i] == 'X') ? 1 : 0;
-        maxSize = max(maxSize, dp[i][0]);
-        maxSize = max(maxSize, dp[0][i]);
-    }
-
-    // Iterate through the rest of the matrix to fill dp using dynamic programming
-    for (int i = 1; i < n; ++i) {
-        for (int j = 1; j < n; ++j) {
-            if (a[i][j] == 'X') {
-                dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
-                maxSize = max(maxSize, dp[i][j]);
+        // Calculate the number of 'X' horizontally for each row
+        for (int i = 0; i < n; i++) {
+            by_rows[i][0] = (a[i][0] == 'X' ? 1 : 0);
+            for (int j = 1; j < n; j++) {
+                if (a[i][j] == 'X') {
+                    by_rows[i][j] = by_rows[i][j - 1] + 1;
+                } else {
+                    by_rows[i][j] = 0;
+                }
             }
         }
-    }
 
-    return maxSize;
-}
+        // Calculate the number of 'X' vertically for each column
+        for (int j = 0; j < n; j++) {
+            by_cols[0][j] = (a[0][j] == 'X' ? 1 : 0);
+            for (int i = 1; i < n; i++) {
+                if (a[i][j] == 'X') {
+                    by_cols[i][j] = by_cols[i - 1][j] + 1;
+                } else {
+                    by_cols[i][j] = 0;
+                }
+            }
+        }
+
+        int res = 0;
+        
+        // Iterate over each cell from bottom right to top left
+        for (int i = n - 1; i >= res; i--) {
+            for (int j = n - 1; j >= res; j--) {
+                int side = min(by_rows[i][j], by_cols[i][j]);
+                while (side > res) {
+                    // Check if a subsquare of size 'side' can be formed
+                    if (by_rows[i - side + 1][j] >= side &&
+                        by_cols[i][j - side + 1] >= side) {
+                        res = side;
+                    } else {
+                        side--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
