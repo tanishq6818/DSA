d3e3f66467738f1b9279eb43a4cf0d361563dacb
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-03-19 23:08:10 +0530

    Create Leetcode POTD.cpp

---
+++ POTD MARCH 19TH/Leetcode POTD.cpp
@@ -0,0 +1,84 @@
+You are given an array of CPU tasks, each represented by letters A to Z, and a cooling time, n. Each cycle or interval allows the completion of one task. Tasks can be completed in any order, but there's a constraint: identical tasks must be separated by at least n intervals due to cooling time.
+
+​Return the minimum number of intervals required to complete all tasks.
+
+ 
+
+Example 1:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+
+Output: 8
+
+Explanation: A possible sequence is: A -> B -> idle -> A -> B -> idle -> A -> B.
+
+After completing task A, you must wait two cycles before doing A again. The same applies to task B. In the 3rd interval, neither A nor B can be done, so you idle. By the 4th cycle, you can do A again as 2 intervals have passed.
+
+Example 2:
+
+Input: tasks = ["A","C","A","B","D","B"], n = 1
+
+Output: 6
+
+Explanation: A possible sequence is: A -> B -> C -> D -> A -> B.
+
+With a cooling interval of 1, you can repeat a task after just one other task.
+
+Example 3:
+
+Input: tasks = ["A","A","A", "B","B","B"], n = 3
+
+Output: 10
+
+Explanation: A possible sequence is: A -> B -> idle -> idle -> A -> B -> idle -> idle -> A -> B.
+
+There are only two types of tasks, A and B, which need to be separated by 3 intervals. This leads to idling twice between repetitions of these tasks.
+
+ 
+
+Constraints:
+
+1 <= tasks.length <= 104
+tasks[i] is an uppercase English letter.
+0 <= n <= 100
+  class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        // Building frequency map
+        int freq[26] = {0};
+        for (char &ch : tasks) {
+            freq[ch - 'A']++;
+        }
+
+        // Max heap to store frequencies
+        priority_queue<int> pq;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.push(freq[i]);
+            }
+        }
+
+        int time = 0;
+        // Process tasks until the heap is empty
+        while (!pq.empty()) {
+            int cycle = n + 1;
+            vector<int> store;
+            int taskCount = 0;
+            // Execute tasks in each cycle
+            while (cycle-- && !pq.empty()) {
+                if (pq.top() > 1) {
+                    store.push_back(pq.top() - 1);
+                }
+                pq.pop();
+                taskCount++;
+            }
+            // Restore updated frequencies to the heap
+            for (int &x : store) {
+                pq.push(x);
+            }
+            // Add time for the completed cycle
+            time += (pq.empty() ? taskCount : n + 1);
+        }
+        return time;
+    }
+};
