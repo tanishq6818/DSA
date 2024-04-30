2830c01c0d6de91acf385874903cfdc095327892
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-04-30 13:53:55 +0530

    Create GFG POTD.cpp

---
+++ POTD APRIL 30TH/GFG POTD.cpp
@@ -0,0 +1,64 @@
+Given two decimal numbers, num1 and num2, represented by linked lists of size n and m respectively. The task is to return a linked list that represents the sum of these two numbers.
+
+For example, the number 190 will be represented by the linked list, 1->9->0->null, similarly 25 by 2->5->null. Sum of these two numbers is 190 + 25 = 215, which will be represented by 2->1->5->null. You are required to return the head of the linked list 2->1->5->null.
+
+Note: There can be leading zeros in the input lists, but there should not be any leading zeros in the output list.
+
+Example 1:
+
+Input:
+n = 2
+num1 = 45 (4->5->null)
+m = 3
+num2 = 345 (3->4->5->null)
+Output: 
+3->9->0->null  
+Explanation: 
+For the given two linked list (4 5) and (3 4 5), after adding the two linked list resultant linked list will be (3 9 0).
+Example 2:
+
+Input:
+n = 4
+num1 = 0063 (0->0->6->3->null)
+m = 2
+num2 = 07 (0->7->null)
+Output: 
+7->0->null
+Explanation: 
+For the given two linked list (0 0 6 3) and (0 7), after adding the two linked list resultant linked list will be (7 0).
+Your Task:
+The task is to complete the function addTwoLists() which has node reference of both the linked lists and returns the head of the sum list.   
+
+Expected Time Complexity: O(n+m)
+Expected Auxiliary Space: O(max(n,m)) for the resultant list.
+
+Constraints:
+1 <= n, m <= 104class Solution {
+public:
+    // Function to add two numbers represented by linked list.
+    Node* addTwoLists(Node* num1, Node* num2) {
+        Node* resultHead = new Node(0); // Dummy head node for the result list
+        Node* current = resultHead; // Pointer to traverse the result list
+        int carry = 0; // Carry for addition
+
+        while (num1 || num2 || carry) {
+            int sum = carry;
+            if (num1) {
+                sum += num1->data;
+                num1 = num1->next;
+            }
+            if (num2) {
+                sum += num2->data;
+                num2 = num2->next;
+            }
+            carry = sum / 10; // Calculate the carry
+            sum %= 10; // Calculate the digit to be added to the result
+
+            // Create a new node with the calculated digit and add it to the result list
+            current->next = new Node(sum);
+            current = current->next;
+        }
+
+        return resultHead->next; // Return the head of the result list (skipping the dummy head node)
+    }
+};
