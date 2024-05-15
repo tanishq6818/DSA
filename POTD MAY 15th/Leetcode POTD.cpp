90325ae8d1e4d7786b8f063b25cedf0efa42ab83
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-05-15 21:38:18 +0530

    Create Leetcode POTD.cpp

---
+++ POTD MAY 15th/Leetcode POTD.cpp
@@ -0,0 +1,164 @@
+You are given a 0-indexed 2D matrix grid of size n x n, where (r, c) represents:
+
+A cell containing a thief if grid[r][c] = 1
+An empty cell if grid[r][c] = 0
+You are initially positioned at cell (0, 0). In one move, you can move to any adjacent cell in the grid, including cells containing thieves.
+
+The safeness factor of a path on the grid is defined as the minimum manhattan distance from any cell in the path to any thief in the grid.
+
+Return the maximum safeness factor of all paths leading to cell (n - 1, n - 1).
+
+An adjacent cell of cell (r, c), is one of the cells (r, c + 1), (r, c - 1), (r + 1, c) and (r - 1, c) if it exists.
+
+The Manhattan distance between two cells (a, b) and (x, y) is equal to |a - x| + |b - y|, where |val| denotes the absolute value of val.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[1,0,0],[0,0,0],[0,0,1]]
+Output: 0
+Explanation: All paths from (0, 0) to (n - 1, n - 1) go through the thieves in cells (0, 0) and (n - 1, n - 1).
+Example 2:
+
+
+Input: grid = [[0,0,1],[0,0,0],[0,0,0]]
+Output: 2
+Explanation: The path depicted in the picture above has a safeness factor of 2 since:
+- The closest cell of the path to the thief at cell (0, 2) is cell (0, 0). The distance between them is | 0 - 0 | + | 0 - 2 | = 2.
+It can be shown that there are no other paths with a higher safeness factor.
+Example 3:
+
+
+Input: grid = [[0,0,0,1],[0,0,0,0],[0,0,0,0],[1,0,0,0]]
+Output: 2
+Explanation: The path depicted in the picture above has a safeness factor of 2 since:
+- The closest cell of the path to the thief at cell (0, 3) is cell (1, 2). The distance between them is | 0 - 1 | + | 3 - 2 | = 2.
+- The closest cell of the path to the thief at cell (3, 0) is cell (3, 2). The distance between them is | 3 - 3 | + | 0 - 2 | = 2.
+It can be shown that there are no other paths with a higher safeness factor.
+ 
+
+Constraints:
+
+1 <= grid.length == n <= 400
+grid[i].length == n
+grid[i][j] is either 0 or 1.
+There is at least one thief in the grid.
+
+class Solution {
+public:
+
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        queue<pair<int, int>> multiSourceQueue;
+        // Mark thieves as 0 and empty cells as -1, and push thieves to the
+        // queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    // Push thief coordinates to the queue
+                    multiSourceQueue.push({i, j});
+                    // Mark thief cell with 0
+                    grid[i][j] = 0;
+                } else {
+                    // Mark empty cell with -1
+                    grid[i][j] = -1;
+                }
+            }
+        }
+
+        // Calculate safeness factor for each cell using BFS
+        while (!multiSourceQueue.empty()) {
+            int size = multiSourceQueue.size();
+            while (size-- > 0) {
+                auto curr = multiSourceQueue.front();
+                multiSourceQueue.pop();
+                // Check neighboring cells
+                for (auto& d : dir) {
+                    int di = curr.first + d[0];
+                    int dj = curr.second + d[1];
+                    int val = grid[curr.first][curr.second];
+                    // Check if the cell is valid and unvisited
+                    if (isValidCell(grid, di, dj) && grid[di][dj] == -1) {
+                        // Update safeness factor and push to the queue
+                        grid[di][dj] = val + 1;
+                        multiSourceQueue.push({di, dj});
+                    }
+                }
+            }
+        }
+
+        // Binary search for maximum safeness factor
+        int start = 0;
+        int end = 0;
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Set end as the maximum safeness factor possible
+                end = max(end, grid[i][j]);
+            }
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isValidSafeness(grid, mid)) {
+                // Store valid safeness and search for larger ones
+                res = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return res;
+    }
+
+private:
+
+    // Directions for moving to neighboring cells: right, left, down, up
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    // Check if a given cell lies within the grid
+    bool isValidCell(vector<vector<int>>& grid, int i, int j) {
+        int n = grid.size();
+        return i >= 0 && j >= 0 && i < n && j < n;
+    }
+
+    // Check if a path exists with given minimum safeness value
+    bool isValidSafeness(vector<vector<int>>& grid, int minSafeness) {
+        int n = grid.size();
+
+        // Check if the source and destination cells satisfy minimum safeness
+        if (grid[0][0] < minSafeness || grid[n - 1][n - 1] < minSafeness) {
+            return false;
+        }
+
+        queue<pair<int, int>> traversalQueue;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        traversalQueue.push({0, 0});
+        visited[0][0] = true;
+
+        // Breadth-first search to find a valid path
+        while (!traversalQueue.empty()) {
+            auto curr = traversalQueue.front();
+            traversalQueue.pop();
+            if (curr.first == n - 1 && curr.second == n - 1) {
+                return true; // Valid path found
+            }
+            // Check neighboring cells
+            for (auto& d : dir) {
+                int di = curr.first + d[0];
+                int dj = curr.second + d[1];
+                // Check if the neighboring cell is valid and unvisited
+                if (isValidCell(grid, di, dj) && !visited[di][dj] &&
+                    grid[di][dj] >= minSafeness) {
+                    visited[di][dj] = true;
+                    traversalQueue.push({di, dj});
+                }
+            }
+        }
+
+        return false; // No valid path found
+    }
+};
