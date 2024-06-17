4dc5a88e44b2502560f2abaec6b19e2fabb9e6f7
Tanishq Agarwal <tanishq.agrawal11@yahoo.com>
2024-06-17 11:51:11 +0530

    Create Leetcode POTD.cpp

---
+++ POTD JUN 17TH/Leetcode POTD.cpp
@@ -0,0 +1,33 @@
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+ 
+
+Example 1:
+
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+Example 2:
+
+Input: c = 3
+Output: false
+ 
+
+Constraints:
+
+0 <= c <= 231 - 1
+
+  bool judgeSquareSum(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
+        }
+        return c % 4 != 3;
+    }
